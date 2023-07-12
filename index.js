f7e1b54b9d61beafe0d3a537a54e0a9580ebd003
Ikenna <enebret@yahoo.com>
2023-07-12 13:12:35 +0100

    tested client and its associated methods. see (https://docs.wwebjs.dev/Client.html#sendMessage) for reference

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const fs = require("fs");
 const qrcode = require('qrcode-terminal');
 const { Console } = require("console");
+
 const Logger = new Console({
     stdout: fs.createWriteStream("normalStdout.txt", { flags: 'a' }),
     stderr: fs.createWriteStream("errStdErr.txt"),
@@ -22,7 +23,12 @@ client.on("authenticated", (session) => {
 
   client.on('ready', () => {
     console.log('Client is ready!');
-    getUnreadMsg(client);
+    const text = "Alright";
+    const chatId = '2348108093964@c.us';
+    const groupId = '120363045867794165@g.us';
+    client.sendMessage(groupId, text);
+    
+    //getUnreadMsg(client);
 });
 
 client.on("auth_failure", (session) => {
